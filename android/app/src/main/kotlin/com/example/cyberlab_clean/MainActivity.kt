6e5aa0652158ef1c1120b5b6bff02ba1b8b1946d
prbryso <brysonp1956@gmail.com>
2026-05-16 15:59:45 -0700

    update
    update

--- android/app/src/main/kotlin/com/example/cyberlab_clean/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cyberlab_clean
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/responsive/breakpoints.dart
+++
@@ -1,5 +0,0 @@
-class Breakpoints {
-  static const double small = 600;   // phones
-  static const double medium = 1024; // tablets
-  static const double large = 1440;  // desktops
-}
